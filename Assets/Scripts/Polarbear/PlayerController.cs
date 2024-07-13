1cccc43c6adea49e029a73a22111f44b57f9a4ec
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-13 21:12:44 +0900

    feat: made polarbear scene room layout

---
+++ Assets/Scripts/Polarbear/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public InputAction MoveAction;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = MoveAction.ReadValue<Vector2>();
+    }
+
+    void FixedUpdate() {
+        Vector2 position = (Vector2)rigidbody2d.position + 3.0f * move * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+}
