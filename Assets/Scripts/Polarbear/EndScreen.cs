91c10e5937fea0f762fc81ec7c866e16bf89771b
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-15 15:31:38 +0900

    feat: connected to polarbear end scene

--- Assets/Scripts/Polarbear/EndScreen.cs
+++ Assets/Scripts/Polarbear/EndScreen.cs
@@ -1,20 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class EndScreen : MonoBehaviour
 {
     public GameObject continueText;
+    public InputAction endAction;
     IEnumerator ShowContinueText()
     {
         yield return new WaitForSeconds(2.0f);
+        endAction.Enable();
         while (true) {
             if (continueText.activeInHierarchy) {
                 continueText.SetActive(false);
             } else {
                 continueText.SetActive(true);
             }
-            yield return new WaitForSeconds(1.0f);
+            yield return new WaitForSeconds(0.75f);
         }        
     }
     // Start is called before the first frame update
@@ -26,6 +30,9 @@ public class EndScreen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (endAction.enabled && endAction.IsPressed()) {
+            StopAllCoroutines();
+            SceneManager.LoadScene("PolarbearEndingScene");
+        }
     }
 }
