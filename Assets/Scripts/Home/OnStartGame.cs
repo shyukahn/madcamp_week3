68948f2dea26924c9f4db71008a3bd2e786137e8
Sehyuk Ahn <shyukahn@gmail.com>
2024-07-17 20:32:14 +0900

    fix: fix screen resolution

---
+++ Assets/Scripts/Home/OnStartGame.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnStartGame : MonoBehaviour
+{
+    void Awake() {
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
